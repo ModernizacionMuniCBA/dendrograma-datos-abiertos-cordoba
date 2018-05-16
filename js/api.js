acc8d18a7ca56d3dd629e352873120f9e424d823
gastonframirez <gastonframirez@gmail.com>
2018-05-16 03:43:43 -0300

    Empiezo con nueva version basada en categorias
    Y arreglo error de HTTP

--- js/api.js
+++ js/api.js
@@ -1,10 +1,10 @@
-var datosURL = "http://gobiernoabierto.cordoba.gob.ar";
+var datosURL = "https://gobiernoabierto.cordoba.gob.ar";
 
 var datosTask = new Promise(function (resolve, reject) {
     var today = new Date().toLocaleDateString();
 
     if (localStorage.datosData == null || localStorage.datosData == "null" || localStorage.datosData == "undefined" || localStorage.userDate != today ) {
-        d3.json(datosURL + "/api/datos-abiertos/?format=json&page_size=550", function (error, datos) {
+        d3.json(datosURL + "/api/categorias-datos-abiertos/?format=json&page_size=10s0", function (error, datos) {
             if (error) reject(error);
             resolve(datos);
             localStorage.datosData = JSON.stringify(datos);
--- js/parser.js
+++ js/parser.js
@@ -3,64 +3,44 @@ function generateTree(array, level) {
             "name": "Datos Abiertos",
             "link": "/data/datos-abiertos",
             "size": level,
-            "children": generateChildrenTree(array, level)
+            "children": generateChildrenTree(array, "Datos Abiertos", level)
     }];
     return array_root;
 }
-function generateChildrenTree(array, level) {
-  var arr = [];
-
-  for(var i=0; i < array.length; i++){
-    arr.push(createDataSet(array[i], level+1));
-  }
-  return arr;
+function generateChildrenTree(array, parent, level) {
+  array = _.each(array, function (catData) {
+          if(catData.depende_de == null){
+            catData.depende_de = "Datos Abiertos";
+          }
+      });
+  return _.map(getSubordinates(array, parent), function (catData) {
+          var children = generateChildrenTree(array, catData.nombre, level + 1);
+          var catData = createDataSet(catData, children, level);
+          return catData;
+      }
+  );
 }
 
-function createDataSet(datum, level) {
-    var name = datum.titulo;
+
+function createDataSet(datum, children, level) {
+    var name = datum.nombre;
     var link = datum.url;
-    var arrCh = [];
-    if (datum.versiones.length != 0) {
-        for(var i=0; i < datum.versiones.length; i++){
-          arrCh.push(createVer(datum.versiones[i], level+1));
-        }
+    if (children.length != 0) {
         return {
             name: name,
-            children: arrCh,
+            children: children,
             size: level,
             link: link,
             data: datum
         };
     } else {
-        return {name: fullName, size: level, link: link, data: datum};
+        return {name: name, size: level, link: link, data: datum};
     }
     return null;
 }
 
-function createVer(version, level) {
-    var name = version.titulo;
-    var link = version.url;
-    var arrCh = [];
-    if (version.recursos.length != 0) {
-      for(var i=0; i < version.recursos.length; i++){
-        arrCh.push(createRec(version.recursos[i], level+1));
-      }
-      return {
-          name: name,
-          children: arrCh,
-          size: level,
-          link: link,
-          data: version
-      };
-    } else {
-        return {name: name, size: level, link: link, data: version};
-    }
-    return null;
-}
-
-function createRec(resource, level) {
-    var name = resource.titulo;
-    var link = resource.url;
-    var icon = resource.icon;
-    return {name: name, size: level, icon: icon, link: link, data: resource};
+function getSubordinates(array, parent) {
+    return _.filter(array, function(datum) {
+      return datum.depende_de == parent;
+    });
 }
